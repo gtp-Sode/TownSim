f995aae53d1ca7f01c69140c340a37084d28cc0f
Tapetklister <42680887+gtp-Sode@users.noreply.github.com>
2021-02-26 22:44:27 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace townSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Town simulator by Sode Timmerås: \n");
+
+            List<string> actions = new List<string>();
+
+
+            float foodConsumption = 0f;
+            float food = 0f;
+
+            int farms = 0;
+            float farmYield = 0f;
+
+            int population = 0;
+            float happiness = 0f;
+
+            bool GameLoop = true;
+            while (GameLoop)
+            {
+
+            }
+
+        }
+
+        static float 
+    }
+}
